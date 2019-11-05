deeb80826a4c17ba6b74e6f2e2a06e6784d5df11
Nikos Kyknas <nkuknas@gmail.com>
2019-11-05 22:35:46 +0000

    auto generation of map ids

--- pet-clinic-data/src/main/java/com/chiknas/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/services/map/AbstractMapService.java
@@ -1,34 +1,45 @@
 package com.chiknas.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.chiknas.petclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * @author nkukn - created on 29-Oct-19 9:35 PM
  */
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id){
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Objects can not be null");
+        }
+
         return object;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object){
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        return !map.keySet().isEmpty() ? Collections.max(map.keySet()) + 1 : 1L;
     }
 }
--- pet-clinic-web/src/main/java/com/chiknas/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/chiknas/petclinic/bootstrap/DataLoader.java
@@ -26,25 +26,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Nikos");
         owner1.setLastName("Kyknas");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Giwrgos");
         owner2.setLastName("Kyknas");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("NikosVet");
         vet1.setLastName("KyknasVet");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("GiwrgosVet");
         vet2.setLastName("KyknasVet");
         vetService.save(vet2);
