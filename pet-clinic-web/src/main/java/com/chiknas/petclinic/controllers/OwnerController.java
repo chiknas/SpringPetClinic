28d6f8bc8c79a1990aed81957740b843b77466c5
Nikos Kyknas <nkuknas@gmail.com>
2019-11-05 22:19:18 +0000

    implement vets/owners lists

--- pet-clinic-web/src/main/java/com/chiknas/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/chiknas/petclinic/controllers/OwnerController.java
@@ -1,6 +1,9 @@
 package com.chiknas.petclinic.controllers;
 
+import com.chiknas.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerMapService) {
+        this.ownerService = ownerMapService;
+    }
+
     @GetMapping({"", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
