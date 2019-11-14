ac4a05e6b4e6cbe7a50ac24ec7d305cbbe2a5f29
Nikos Kyknas <nkuknas@gmail.com>
2019-11-14 22:14:36 +0000

    create visit entity

--- pet-clinic-data/src/main/java/com/chiknas/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package com.chiknas.petclinic.model;
 
+import java.util.Set;
+
 /**
  * @author nkukn - created on 19-Oct-19 9:40 PM
  */
 public class Owner extends Person {
+    Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
