14ab8c0ceb889f30e5ac9b157d1004d2f7733dc6
Nikos Kyknas <nkuknas@gmail.com>
2019-11-17 21:33:17 +0000

    implement petType map service

---
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.chiknas.petclinic.services;
+
+import com.chiknas.petclinic.model.PetType;
+
+/**
+ * @author nkukn - created on 17/11/2019 9:30 μ.μ.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package com.chiknas.petclinic.services.map;
+
+import com.chiknas.petclinic.model.PetType;
+import com.chiknas.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author nkukn - created on 17/11/2019 9:31 μ.μ.
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
