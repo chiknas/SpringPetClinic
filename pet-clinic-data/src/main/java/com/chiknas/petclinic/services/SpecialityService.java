08ebd794564f1dd95ec15fd928cf076ae0b4c853
Nikos Kyknas <nkuknas@gmail.com>
2019-11-17 22:08:03 +0000

    implement specialities map

---
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.chiknas.petclinic.services;
+
+import com.chiknas.petclinic.model.Speciality;
+
+/**
+ * @author nkukn - created on 17/11/2019 10:04 μ.μ.
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.chiknas.petclinic.services.map;
+
+import com.chiknas.petclinic.model.Speciality;
+import com.chiknas.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+/**
+ * @author nkukn - created on 17/11/2019 10:04 μ.μ.
+ */
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
