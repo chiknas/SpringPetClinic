ed4aafa06488ed13e3493c8cdd88da159c6bd4fa
Nikos Kyknas <nkuknas@gmail.com>
2019-11-17 21:29:21 +0000

    added owner contact info

--- pet-clinic-data/src/main/java/com/chiknas/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/chiknas/petclinic/model/Owner.java
@@ -6,7 +6,34 @@ import java.util.Set;
  * @author nkukn - created on 19-Oct-19 9:40 PM
  */
 public class Owner extends Person {
-    Set<Pet> pets;
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
